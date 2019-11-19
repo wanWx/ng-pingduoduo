4833d06b6be918489500e149f81d4a8e8ca5dc42
wanwenxuan <wanwenxuanli@foxmail.com>
2019-11-19 13:38:07 +0800

    2-6 【组件封装】分离封装第一个组件

---
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-scrollable-tab',
+  templateUrl: './scrollable-tab.component.html',
+  styleUrls: ['./scrollable-tab.component.scss']
+})
+export class ScrollableTabComponent implements OnInit {
+
+  selectedIndex = -1;
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '电脑',
+      link: ''
+    },
+    {
+      title: '内衣',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
