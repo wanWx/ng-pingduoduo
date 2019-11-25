fb85a08027b03837082b0e99f7f26632a278133f
wanwenxuan <wanwenxuanli@foxmail.com>
2019-11-25 17:31:12 +0800

    详情页

--- src/app/product/components/product-container/product-container.component.ts
+++ src/app/product/components/product-container/product-container.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  OnDestroy
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { ProductVariant } from '../../domain';
+import { OrderService } from '../../services';
 
 @Component({
   selector: 'app-product-container',
   templateUrl: './product-container.component.html',
-  styleUrls: ['./product-container.component.scss']
+  styleUrls: ['./product-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductContainerComponent implements OnInit {
-
-  constructor() { }
+export class ProductContainerComponent implements OnInit, OnDestroy {
+  variants$: Observable<ProductVariant[]>;
+  selectedIndex = 0;
+  subs: Subscription[] = [];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private orderService: OrderService,
+  ) {}
 
   ngOnInit() {
+    const productId$ = this.route.paramMap.pipe(
+      filter(params => params.has('productId')),
+      map(params => params.get('productId'))
+    );
+    this.variants$ = productId$.pipe(
+      switchMap(productId =>
+        this.orderService.getProductVariantsByProductId(productId)
+      )
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
+    this.subs = [];
+  }
+
+  handleDirectBuy(variants: ProductVariant[]) {}
+
+  handleGroupBuy(variants: ProductVariant[]) {
+    const top = 40;
+    // 传入 Output，EventEmitter 其实就是一个 Subject
+    const formSubmitted = new EventEmitter();
+    this.subs.push(
+      formSubmitted.subscribe(ev => {
+        this.router.navigate(['/orders', 'confirm']);
+      })
+    );
+    const selected = new EventEmitter<number>();
+    this.subs.push(
+      selected.subscribe(idx => {
+        this.selectedIndex = idx;
+      })
+    );
+  }
 }
--- src/app/product/index.ts
+++ src/app/product/index.ts
@@ -1,2 +1,4 @@
 export * from './product.module';
 export * from './components';
+export * from './domain';
+export * from './services';
